c26e33f30d567e01d1dcb19cec52ad29c1bb9088
Dexter Vandersall <dex.vandersall@gmail.com>
2018-09-26 14:22:29 -0400

    .

---
+++ player.py
@@ -0,0 +1,15 @@
+
+def Player():
+
+    """
+        player needs:
+            ships
+            energy hub stats
+                node counter
+                i/o
+                total energy loss
+                
+                
+        
+        
+    """
